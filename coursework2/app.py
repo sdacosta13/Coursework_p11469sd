ddde798fb50592616f0d260e2037fda055ec0255
Sam Da Costa <samuel.dacosta-2@student.manchester.ac.uk>
2019-11-28 15:29:18 +0000

    rotation script needs work

--- coursework2/app.py
+++ coursework2/app.py
@@ -1,11 +1,12 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
+import math
 import random
 """
 Written By Sam da Costa
 References
 character sprites: https://davidjakubec.itch.io/characters-sprite-sheet
+Dungeon sprites: https://0x72.itch.io/dungeontileset-ii
 """
 
 hitsOn = True
@@ -25,6 +26,8 @@ class App():
     def __init__(self):
         self.width = 1920
         self.height = 1080
+        self.mouseX = 0
+        self.mouseY = 0
         self.window = Tk()
         self.window.title("Game")
         self.window.resizable(0,0)
@@ -40,6 +43,7 @@ class App():
         self.canvas.itemconfigure(self.tickText, text = str(self.ticks))
         self.canvas.pack()
         char.move()
+        char.weapon.pointAtMouse()
         self.window.after(self.loopSpeed,self.gameloop)
 
     def setBinds(self):
@@ -48,12 +52,15 @@ class App():
     def main(self):
         self.canvas.bind("<KeyPress>", self.keyPressedHandler)
         self.canvas.bind("<KeyRelease>", self.releaseHandler)
+        self.canvas.bind("<Motion>", self.handleMouseMovement)
         self.gameloop()
         self.canvas.pack()
         self.window.mainloop()
 
+    def handleMouseMovement(self, event):
+        self.mouseX, self.mouseY = event.x, event.y
+
     def keyPressedHandler(self, event):
-        print(event.keycode)
         """windowsKeys
         uKey = 87
         lKey = 65
@@ -98,6 +105,80 @@ class App():
         if event.keycode == rKey:
             char.rightRelease()
 
+class Weapon:
+    def __init__(self, x, y):
+        global app
+        self.rotation = 0
+        width = 10
+        length = 70
+        self.x = x
+        self.y = y
+        self.coords = [x-(width/2), y-(width/2),
+        x+length, y-(width/2),
+        x+length, y+(width/2),
+        x-(width/2), y+(width/2)
+
+        ]
+
+        self.skin = app.canvas.create_polygon(self.coords,fill = "brown",outline="")
+    def rotate(self, angle):
+        temp = self.coords
+        for i in range(0,len(temp),2):
+            #move shape to origin then use matrix multiplication to rotate
+            temp[i] -= self.x
+            temp[i+1] -= self.y
+            new = []
+            #aliases
+
+            x = temp[i]
+            y = temp[i+1]
+            new.append((x*math.cos(angle))-(y*math.sin(angle)))
+            new.append((x*math.sin(angle))+(y*math.cos(angle)))
+
+            temp[i] = new[0]
+            temp[i+1] = new[1]
+
+            #move back
+            temp[i] += self.x
+            temp[i+1] += self.y
+
+        self.coords = temp
+        app.canvas.coords(self.skin, self.coords)
+        self.rotation += angle
+    def move(self,x,y):
+        app.canvas.move(self.skin, x, y)
+        self.x += x
+        self.y += y
+
+    def pointAtMouse(self):
+        """
+        if (app.mouseX-self.x != 0):
+            angle = math.atan((app.mouseY-self.y)/(app.mouseX-self.x))
+            print(angle * 180 / math.pi)
+            self.rotate(angle-self.rotation)
+            self.rotation = angle
+        """
+        pi = math.pi
+        self.rotate(-self.rotation)
+        print(self.rotation)
+        if (app.mouseX-self.x != 0):
+            angle = math.atan((app.mouseY-self.y)/(app.mouseX-self.x))
+
+            if app.mouseY <= self.y and app.mouseX >= self.x:
+                #first quad
+                self.rotate(angle)
+            elif app.mouseY >= self.y and app.mouseX >= self.x:
+                #second quad
+                self.rotate(angle)
+            elif app.mouseY >= self.y and app.mouseX <= self.x:
+                #third quad
+                self.rotate(angle+pi)
+            elif app.mouseY <= self.y and app.mouseX <= self.x:
+                #fourth quad
+                self.rotate(angle-pi)
+
+
+
 
 
 
@@ -120,7 +201,6 @@ class Sprite:
         app.canvas.tag_lower(self.hitbox)
 
 
-
 class Character(Sprite):
     def __init__(self, image, x, y, showHitbox = False):
         Sprite.__init__(self, image, x, y, showHitbox = showHitbox)
@@ -129,7 +209,8 @@ class Character(Sprite):
         self.cLeft = PhotoImage(master = app.canvas, file = "cLeft.png")
         self.cRight = PhotoImage(master = app.canvas, file = "cRight.png")
         self.cBehind = PhotoImage(master = app.canvas, file = "cBehind.png")
-
+        self.weapon = Weapon(x,y)
+        self.weapon.rotation = 0
 
     def upPressed(self):
         self.movement[0] = 1
@@ -155,34 +236,51 @@ class Character(Sprite):
 
     def upRelease(self):
         self.movement[0] = 0
+        self.updateSkin()
 
 
     def leftRelease(self):
         self.movement[1] = 0
-
+        self.updateSkin()
 
     def downRelease(self):
         self.movement[2] = 0
-
+        self.updateSkin()
 
     def rightRelease(self):
         self.movement[3] = 0
+        self.updateSkin()
 
     def move(self):
         if self.movement[0]:
             app.canvas.move(self.img, 0, -self.moveSpeed)
             app.canvas.move(self.hitbox, 0, -self.moveSpeed)
+            self.weapon.move(0, -self.moveSpeed)
+            self.weapon.y -= self.moveSpeed
         if self.movement[1]:
             app.canvas.move(self.img, -self.moveSpeed, 0)
             app.canvas.move(self.hitbox, -self.moveSpeed, 0)
+            self.weapon.move(-self.moveSpeed, 0)
+            self.weapon.x -= self.moveSpeed
         if self.movement[2]:
             app.canvas.move(self.img, 0, self.moveSpeed)
             app.canvas.move(self.hitbox, 0, self.moveSpeed)
+            self.weapon.move(0, self.moveSpeed)
+            self.weapon.y += self.moveSpeed
         if self.movement[3]:
             app.canvas.move(self.img, self.moveSpeed, 0)
             app.canvas.move(self.hitbox, self.moveSpeed, 0)
-
-
+            self.weapon.move(self.moveSpeed, 0)
+            self.weapon.x += self.moveSpeed
+    def updateSkin(self):
+        if self.movement[0]:
+            app.canvas.itemconfigure(self.img, image = self.cBehind)
+        elif self.movement[1]:
+            app.canvas.itemconfigure(self.img, image = self.cLeft)
+        elif self.movement[2]:
+            app.canvas.itemconfigure(self.img, image = self.cFront)
+        elif self.movement[3]:
+            app.canvas.itemconfigure(self.img, image = self.cRight)
 
 
 
